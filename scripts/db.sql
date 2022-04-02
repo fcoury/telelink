53cfc044eb8786ef52230e4a40b6b26a541c4673
Felipe Coury <felipe.coury@gmail.com>
2022-04-02 10:28:56 -0300

    feat: web server uses postgres

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE telelink;
-
 CREATE TABLE links (
   "id" SERIAL PRIMARY KEY,
   "url" TEXT NOT NULL,
   "title" TEXT,
   "text" TEXT,
   "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+  "viewed_at" TIMESTAMP(3)
 );
